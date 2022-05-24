57f9ce9f80abab8bd85b548de3067a75449e2e5a
PrakharSharma888 <prakharentp2002@gmail.com>
2022-05-24 10:56:55 +0530

    Updated contract address

--- constrants/index.js
+++ constrants/index.js
@@ -2,6 +2,6 @@
 
 const WHITELIST_CONTRACT_ADDRESS = "0x717Afe255f820527476A9aE4C1fF47B99Fb17D94"
 
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-crypto-dev.vercel.app/api/";
 
 module.exports = {WHITELIST_CONTRACT_ADDRESS, METADATA_URL}
