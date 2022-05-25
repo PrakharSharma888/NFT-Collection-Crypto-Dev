26219fa4bfd9c8ac8264530c0365c6b59c0fc75e
PrakharSharma888 <prakharentp2002@gmail.com>
2022-05-25 21:24:12 +0530

    Updated Images

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   const tokenId = req.query.tokenId
   const name = `Crypto Dev #${tokenId}`;
   const description = "Crypto Devs is a NFT collection made by Prakhar Sharma";
-  const image = `https://raw.githubusercontent.com/PrakharSharma888/NFT-Collection-Crypto-Dev/main/my-app/public/4.svg`;
+  const image = `https://raw.githubusercontent.com/PrakharSharma888/NFT-Collection-Crypto-Dev/main/my-app/public/${tokenId}.svg`;
 
     return res.json({
       name : name,
